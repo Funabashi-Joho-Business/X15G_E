15d64a27205f47e2a0c3cef2c6db47e1a2d58338
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-10-16 12:28:17 +0900

    サンプルアドレスの追加

--- app/src/main/java/jp/ac/chiba_fjb/oikawa/routetest/RouteData.java
+++ app/src/main/java/jp/ac/chiba_fjb/oikawa/routetest/RouteData.java
@@ -1,6 +1,8 @@
 package jp.ac.chiba_fjb.oikawa.routetest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//リターンデータのサンプル
+//https://maps.googleapis.com/maps/api/directions/json?language=ja&origin=%E5%8D%83%E8%91%89&destination=%E6%9D%B1%E4%BA%AC&mode=driving&sensor=false
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class RouteData{
