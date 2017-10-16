159d89d3682c71f692ca619ca6937fc323b91a4b
WIN_NT\x15g012 <x15g012@chiba-fjb.ac.jp>
2017-10-16 14:43:14 +0900

    横スライドサンプルテスト実装

--- app/src/main/java/jp/ac/chiba_fjb/e/e_graduate_test/MapsActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/e/e_graduate_test/MapsActivity.java
@@ -1,7 +1,13 @@
 package jp.ac.chiba_fjb.e.e_graduate_test;
 
+import android.content.res.Configuration;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,21 +16,50 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        mDrawer = (DrawerLayout)((ViewGroup)findViewById(android.R.id.content)).getChildAt(0);
+        mToggle = new ActionBarDrawerToggle(this, mDrawer, R.string.app_name, R.string.app_name);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Activityの開始が完了したとき、ActionBarDrawerToggleの状態を同期させる
+        mToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Activityが変更した時に、ActionBarDrawerToggleも変更させる
+        mToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)) {
+            // ボタンが押された時、ActionBarDrawerToggleにNavigationDrawerの開閉させる
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
